ff02d86a523a4629442df7d0085720fc22d45441
Justina Bui <70236275+buijustina@users.noreply.github.com>
2020-11-03 11:31:07 -1000

    Lab 13 Update 11.3

--- Lab13+4/Lab13_Final/info_server_Ex2.js
+++ Lab13+4/Lab13_Final/info_server_Ex2.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+
+app.get('/test', function (request, response, next) {
+    response.send("Got a GET to/test path");
+    next();
+});
+
 app.all('*', function (request, response, next) {
-    response.send(request.method + ' to path ' + request.path);
+    console.log(request.method + ' to path ' + request.path);
 });
 app.listen(8080, () => console.log(`listening on port 8080`)); 
 // note the use of an anonymous function here
