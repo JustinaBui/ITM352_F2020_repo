32c347cb9056faabe0f483215d5fe52fb7697f70
Justina Bui <70236275+buijustina@users.noreply.github.com>
2020-11-06 18:57:01 -1000

    As1

---
+++ Bui_Justina/product_data.js
@@ -0,0 +1,42 @@
+products =
+[
+    {  
+    "name":"Brown Sugar Boba",  
+    "price": 4.50,  
+    "image": 'SummerTea.jpg',
+    },
+    {  
+        "name":"Dalgona Milk Tea",  
+        "price": 5.90,  
+        "image": 'DalgonaCoffee.jpg',
+    },
+    {  
+        "name":"English Earl Grey Milk Tea",  
+        "price": 4.99,  
+        "image": 'RabbitRabbit.jpg',
+    },
+    {  
+        "name":"Chocolate Cream Souffle Pancakes",  
+        "price": 11.49,  
+        "image": 'SoufflePc.jpg',
+    },
+    {  
+        "name":"Milk N' Cereal Pancakes",  
+        "price": 14.00,  
+        "image": 'MilknCerealPc.jpg',
+    },
+    {  
+        "name":"Mana Acai Bowl",  
+        "price": 8.00,  
+        "image": 'HaleiwaBowls.jpg',
+    },
+    {  
+        "name":"Strawberry Shortcake Roll-Up Ice Cream",  
+        "price": 7.00,  
+        "image": 'StrawberryShortcase.jpg',
+    }
+  ];
+
+  if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
